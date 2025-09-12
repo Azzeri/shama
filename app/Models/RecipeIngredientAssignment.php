7b03b5403706a12b29e02cab5011c37102740b44
Mariusz <azmario2698@gmail.com>
2025-09-12 18:02:22 +0200

    adding ingredients to recipes implemented

--- app/Models/RecipeIngredientAssignment.php
+++ app/Models/RecipeIngredientAssignment.php
@@ -9,4 +9,11 @@ class RecipeIngredientAssignment extends Pivot
     use HasFactory;
     protected $fillable = ['quantity'];
     protected $table = 'recipe_ingredient_assignments';
+
+    public function ingredients()
+    {
+        return $this->belongsToMany(Ingredient::class, 'recipe_ingredient_assignments')
+            ->withPivot('quantity')
+            ->withTimestamps();
+    }
 }
